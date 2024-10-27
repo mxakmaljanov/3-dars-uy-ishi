7d36f28991e1a30f6165da3cf32488d624ef325c
mxakmaljanov <maxmudxonakmaljanov@gmail.com>
2024-10-27 22:09:44 +0500

    3--darss

---
+++ index.js
@@ -0,0 +1,44 @@
+// 1-masala  //
+//const contentDiv = document.getElementById("content");
+//const paragraph = document.createElement("p");
+//paragraph.textContent = "Bu yangi yaratilgan paragraf.";
+//contentDiv.appendChild(paragraph);
+//
+//2-masala  //
+//const button = document.getElementById("changeText");
+//const textParagraph = document.getElementById("text");
+//button.addEventListener("click", function () {
+//  textParagraph.textContent = "Sarvar";
+//});
+//4-masala //
+//document.getElementById("styleButton").addEventListener("click", function () {
+//  const styledText = document.getElementById("styledText");
+//  styledText.style.color = "blue";
+//  styledText.style.fontSize = "24px";
+//});
+//
+//// 5-masala //
+//document.getElementById("addItem").addEventListener("click", function () {
+//  const itemText = document.getElementById("item").value;
+//  if (itemText) {
+//    const listItem = document.createElement("li");
+//    listItem.textContent = itemText;
+//    document.getElementById("list").appendChild(listItem);
+//    document.getElementById("item").value = "";
+//  }
+//});
+//
+//// 6-masala //
+//document.getElementById("toggleButton").addEventListener("click", function () {
+//  const textElement = document.getElementById("toggleText");
+//  if (textElement.style.display === "none") {
+//    textElement.style.display = "block";
+//  } else {
+//    textElement.style.display = "none";
+//  }
+//});
+//9-masala
+//document.getElementById("colorButton").addEventListener("click", function () {
+//  const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+//  document.body.style.backgroundColor = randomColor;
+//});
